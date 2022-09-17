a484d63441118ba8b150f997b61db9ff9ec1dc12
bethanyalicehall <bethany.hall66@gmail.com>
2022-09-17 09:07:29 +0000

    Added subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,16 +33,16 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    }
-    
-    else {
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
-    };
+    }
 
     
 
-};
+}
 
 /**
  * Checks the answer agaist the first element in
@@ -79,10 +79,10 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"]
-    }
-    
-    else {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -116,7 +116,11 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+
 
 }
 
