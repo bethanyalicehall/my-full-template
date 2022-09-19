1f5c626147e15942be17c412c217bb27c7b3b8c4
bethanyalicehall <bethany.hall66@gmail.com>
2022-09-19 08:44:07 +0000

    Added other division solution option

--- assets/js/script.js
+++ assets/js/script.js
@@ -154,3 +154,14 @@ function displayDivisionQuestion(operand1, operand2) {
         runGame("division");
     }
 }
+//or you could use the below
+//function displayDivisionQuestion(operand1, operand2) {
+    //operand1 = operand1 * operand2;
+    
+    //document.getElementById("operand1").textContent = operand1;
+    //document.getElementById("operand2").textContent = operand2;
+    //document.getElementById("operator").textContent = "/";
+    
+   
+    
+//}
